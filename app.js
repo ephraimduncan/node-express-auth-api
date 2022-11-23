a42a40d603aeedfab765becda8c7f5da28d7d297
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-11-23 13:32:45 +0000

    init express app

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const morgan = require("morgan");
+const createError = require("http-errors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", async (req, res, next) => {
+  res.send("Hello from Express");
+});
+
+app.use(async (req, res, next) => {
+  next(createError.NotFound());
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.send({
+    error: {
+      status: error.status || 500,
+      message: error.message,
+    },
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at: ${PORT}`);
+});
