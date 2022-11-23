252b1120249d98a993fc0ea1927a9e2586037de4
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-11-23 15:04:25 +0000

    feat(auth): Hash new user password

--- Models/User.model.js
+++ Models/User.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -17,6 +19,18 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = mongoose.model("user", UserSchema);
